7c8ca4bc35c5c8dfd647e6d949bcee5caf1000a6
iuuybsy <18803722821@163.com>
2026-04-12 11:06:37 +0800

    Modify katago analysis demo.

--- katago_analysis.py
+++ katago_analysis.py
@@ -2,58 +2,27 @@ import subprocess
 import sys
 
 # 指定KataGo的路径和配置文件
-katago_path = 'D:\\coding\\katago\\katago.exe'
-config_path = 'D:\\coding\\katago\\gtp_human9d_search_example.cfg'
-model_path = 'D:\\coding\\katago\\kata1-b28c512nbt-s7915807488-d4517482653.bin.gz'
-human_model = 'D:\\coding\\katago\\b18c384nbt-humanv0.bin.gz'
+katago_path = 'KataGo\\katago.exe'
+config_path = 'KataGo\\gtp_custom.cfg'
+model_path = 'KataGo\\kata1-b18c384nbt-s9996604416-d4316597426.bin.gz'
+
+print("Path set.")
 
 # 启动KataGo进程
 process = subprocess.Popen([katago_path, 'gtp',
                             '-model', model_path,
-                            '-config', config_path,
-                            '-human-model', human_model],
+                            '-config', config_path],
                            stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
 
+print("Process started.\n\n\n")
 
-def send_command(command):
-    process.stdin.write(command + '\n')
-    process.stdin.flush()
-
-    while True:
-        line = process.stdout.readline()
-        if line.startswith("info"):
-            print(line.strip())
-        elif line == "\n":
-            break
-    return line
-
-# 设置棋盘大小
-send_command('boardsize 19')
-
-# 清空棋盘
-send_command('clear_board')
-send_command('kata-set-rules chinese')
-
-send_command('play b Q16')
-send_command('play w D4')
-send_command('play b Q4')
-send_command('play w D16')
-send_command('play b F3')
-
-# 查询黑棋的最优走法
-best_move = send_command('genmove b')
-print(type(best_move))
-print(f'The best move is: {best_move}')
-
-analysis = send_command('kata-analyze interval 500 maxmoves 3')
-
-print(type(analysis))
-print(f'The analysis is: {analysis}')
-
-final_score = send_command('final_score')
-print(type(final_score))
-print(f'The final score is: {final_score}')
+command = "genmove b"
+process.stdin.write(command + '\n')
+process.stdin.flush()
+feedback = process.stdout.readline()
+print(feedback)
 
 # 关闭KataGo进程
-process.stdin.write('quit\n\n')
-process.communicate()
+process.stdin.write('quit\n')
+process.stdin.flush()
+process.terminate()  # 确保进程结束
